1b8ac92e512739b9c317912cc4a76529ea280f4a
jariftahmid <jariftahmid10@gmail.com>
2025-12-25 18:03:36 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyChrKBpyRSLhkmVMy3c1gdWBp4_grrrphA",
@@ -18,5 +16,3 @@ const app = initializeApp(firebaseConfig);
 
 window.auth = getAuth(app);
 window.db = getFirestore(app);
-window.storage = getStorage(app);
-</script>
