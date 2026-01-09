33dee2a7a60d3fc0b242fd353d90b2a845eefdf3
jariftahmid <jariftahmid10@gmail.com>
2026-01-09 14:15:54 +0600

    Update login.js

--- login.js
+++ login.js
@@ -1,23 +1,24 @@
-import { signInWithEmailAndPassword, setPersistence, browserLocalPersistence } 
+// auth.js
+import { signInWithEmailAndPassword } 
 from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 
-const loginBtn = document.getElementById("loginBtn");
-loginBtn.addEventListener("click", () => {
+import { auth } from "./firebase.js";
+
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", async () => {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  if(!email || !password){
-    alert("Email and password required!");
+  if (!email || !password) {
+    alert("Email and password required");
     return;
   }
 
-  setPersistence(auth, browserLocalPersistence)
-    .then(() => {
-      return signInWithEmailAndPassword(auth, email, password);
-    })
-    .then(() => {
-      alert("Login successful!");
-      window.location.href = "/dashboard.html";
-    })
-    .catch(err => alert(err.message));
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (err) {
+    alert(err.message);
+  }
 });
